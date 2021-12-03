c816e6c9cdc4c38646fc723e585bb81ffb9961ed
ericjcili <ericjcili@gmai.com>
2021-12-02 23:22:26 -0500

    done

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       <div>
         <Header />
         <Switch>
-        <Route exact path='/' component={Home} />
+        <Route exact path='/home' component={Home} />
         <Route exact path='/projects' component={Projects} />
         <Route exact path='/contact' component={Contact} />
         <Route exact path='/resume' component={Resume} />
