7dee34e98da1a3df376016958e7a6a40a84b671a
ericjcili <ericjcili@gmai.com>
2021-12-02 23:17:37 -0500

    added switch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home/Home';
@@ -14,10 +14,12 @@ function App() {
     <Router>
       <div>
         <Header />
+        <Switch>
         <Route exact path='/' component={Home} />
         <Route exact path='/projects' component={Projects} />
         <Route exact path='/contact' component={Contact} />
         <Route exact path='/resume' component={Resume} />
+        </Switch>
         <Footer />
       </div>
     </Router>
